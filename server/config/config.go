2c4a0722344d2be340cd494f37e39ab499507b2e
hizukayudai@gmail.com <09120912Abcd>
2020-05-20 18:28:55 +0900

    fix env

--- server/config/config.go
+++ server/config/config.go
@@ -1,13 +1,9 @@
 package config
 
-import (
-	"fmt"
-	"os"
-)
+import "os"
 
 var DatabaseURL string
 
 func init() {
 	DatabaseURL = os.Getenv("DATABASE_URL") + "?parseTime=true&loc=Local"
-	fmt.Println("=====", os.Getenv("DATABASE_URL"), "======")
 }
